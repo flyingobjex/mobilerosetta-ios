46283d75f9bd9a0bfbfc218080056e0632f6b361
objex <steve@flyingobjex.com>
2018-01-24 15:14:44 -0500

    Initial Commit

---
+++ MobileRosetta/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MobileRosetta
+//
+//  Created by objex on 1/24/18.
+//  Copyright © 2018 mobilerosetta.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
